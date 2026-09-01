272425ebe5408ffa5e281f4b9aa00a24f6b45c53
Thomas Dubuc <2260333@cegepmontpetit.ca>
2026-09-01 10:14:23 -0400

    FCT Exercice 3

---
+++ Migrations/20260901131621_relationGenreGame.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RelationsNaN.Migrations
+{
+    /// <inheritdoc />
+    public partial class relationGenreGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
